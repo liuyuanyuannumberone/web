d409075509a5894518a7514e58db59526f50f271
liuyuanyuan <1445179059@qq.com>
2022-04-13 21:03:11 +0800

    插件学习

--- JavaScript/4.遍历.js
+++ JavaScript/4.遍历.js
@@ -1,8 +1,8 @@
 let arr = [-7, 4, 4, 5, 9, 5, 6, 7];
 
 for (let i = 0; i < arr.length; i++) {}
-for (let key in arr) {}// console.log(arr[key]); //key不是索引  主要为遍历对象设计
-for (let item of arr) {} //可以遍历所有数据结构  主要为遍历可遍历对象设计
+for (let key in arr) {}///key不是索引  主要为遍历对象设计
+for (let item of arr) {} //主要为可遍历对象设计
 
 arr.forEach((item, index) => {});
 arr.every(item => {return item > 0});//只有 return为true 时他才继续遍历，否则停止遍历返回false
---
+++ JavaScript/6.解构赋值.js
@@ -0,0 +1,38 @@
+//对象
+let obj = {
+    type: 'image/png',
+    size: 1432,
+    imgName: '图片名',
+    hobby: ['sleep', 'swim', 'drink'],
+    family: {
+        fa: 'liu',
+        mo: 'wang',
+    },
+};
+const { type, size, imgName } = obj;
+const { type: var1, size: var2, imgName: var3 } = obj;
+let {
+    family: { fa },
+    hobby: [item],
+} = obj;
+// console.log(type, size, imgName,var1, var2, var3,fa,item);
+//赋值
+[obj.size, obj.imgName] = [1000, '新名字'];
+// console.log(obj);
+
+//console.log(Object.entries(obj));
+for (let [k, v] of Object.entries(obj)) {
+    console.log(`${k}:${v}`);
+}
+
+/*
+ *
+ * 数组
+ * */
+let [a, b, c] = [1, 2, 3];
+let [m, [[n], f]] = [1, [[2], 3]];
+
+let [, , q] = [1, 2, 3];
+let [, ...all] = [1, 2, 3, 4];
+
+console.log(all); //[ 2, 3, 4 ]
