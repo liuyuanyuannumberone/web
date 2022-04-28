4ed676ca1cc256ca9d0d62dcf3ee322cf916457c
liuyuanyuan <1445179059@qq.com>
2022-04-28 19:13:00 +0800

    19.12

--- JavaScript/9.set.js
+++ JavaScript/9.set.js
@@ -1,42 +1,21 @@
 //它的成员不重复
 let s = new Set();
 s.add('hello').add('world').add('hello');
-// s.has('hello'); 
+// s.has('hello');
 // s.size
 // s.delete('hello');
 // s.clear();
 // console.log(s.keys());// [Set Iterator] { 'hello', 'world' }
 // console.log(s.values());// [Set Iterator] { 'hello', 'world' }
 // console.log(s.entries());//[Set Entries] { [ 'hello', 'hello' ], [ 'world', 'world' ] }
-s.forEach(item =>{});
-for (let item of s){};
-
-
-
-
-
-/*
-* set去重
-*
-* */
-{
-    let arr = [1, 2, 3, 4, 1, 3, 4, 4, 3, 4, 6, 1, 1];
-    let str = "asasasaddsdsddsdsfdggfgf";
-    console.log([...new Set(arr)]);  //[ 1, 2, 3, 4, 6 ]
-    console.log([...new Set(str)]); //[ 'a', 's', 'd', 'f', 'g' ]
-
+s.forEach(item => {});
+for (let item of s) {
 }
 
+/**
+ * 将set变为数组: Array.from(<Set>)  [...<Set>] 
+ * set去重 new Set(arr)
+ */
 
-/*
-* set并集，交集
-*
-* */
-
-let unionSet = new Set([...[1, 2, 3, 4, 5], ...[6, 7, 8, 9, 10]]);
-console.log(Array.from(unionSet));
-
-
-let intersect = new Set([...new Set([1, 26, 7, 8])].filter(x =>
-    new Set([6, 7, 8, 9, 10]).has(x)));
-console.log(Array.from(intersect));//[7,8]
+let arr = [1, 2, 3, 4, 5,5,6, 7, 8, 9];
+console.log(new Set(arr));
--- JavaScript/函数/01.函数参数.js
+++ JavaScript/函数/01.函数参数.js
@@ -10,7 +10,6 @@ sum(1, 2, 3, 4, 5, 6, 7, 8);
 function add(x = 1, y = 2, z = 3) {
     console.log(x, y, z);
 }
-
 add(...[4, 5, 6]);
 
 
