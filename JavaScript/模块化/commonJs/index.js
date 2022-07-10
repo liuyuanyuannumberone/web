63c123f33f59bec28e537103fc3aee30181b9723
liuyuanyuan <1445179059@qq.com>
2022-07-10 16:03:31 +0800

    提交cookie

--- JavaScript/模块化/commonJs/index.js
+++ JavaScript/模块化/commonJs/index.js
@@ -1,5 +1,5 @@
 let mod1 = require('./mod1.js');
-console.log(mod1, mod1(1, 2), mod1.grade, mod1.person.name);
+// console.log(mod1, mod1(1, 2), mod1.grade, mod1.person.name);
 
 const mod2 = require('./mod2.js');
 mod2.plusCount();
@@ -7,4 +7,4 @@ mod2.plusYuanhua();
 console.log(mod2.count,mod2.friends)// 0 [ 1 ]
 let arr=mod2.friends;
 arr[0]="test"
-console.log(mod2.friends);
+console.log(mod2.friends); //[ 'test' ]
