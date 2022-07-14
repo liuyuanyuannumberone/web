81e36b12a279a63ffdb12446a058d0740d3b3920
liuyuanyuan <1445179059@qq.com>
2022-07-14 23:18:44 +0800

    file 文件学习

--- nodeJS/projects/routes/test.js
+++ nodeJS/projects/routes/test.js
@@ -4,11 +4,12 @@ const _ =require('lodash')
 const url = require('url');
 
 router.get('/getData', function (req, res, next) {
-  // console.log(req); 
+
   res.send({status:200,data:req.query});
 });
 
 router.post('/addData', function (req, res, next) {
+  console.log(req); 
   res.send({status:200,data:1});
 });
 
