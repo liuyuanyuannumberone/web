f3c8481a1c1fea48af8a1ab6f0991f0ef4c9ef27
liuyuanyuan <1445179059@qq.com>
2022-07-16 23:43:51 +0800

    学习ES6的字符串扩展方法

--- JavaScript/字符串/1.字符串.js
+++ JavaScript/字符串/1.字符串.js
@@ -1,22 +1,32 @@
 /**
- * 静态方法
-     1.String.fromCharCode() 参数0-0xFFFF之间的数字  返回这些组成的字符串。
-     不支持码点大于0xFFFF的字符，可以拆分为多个字节表示;
-  实例方法   
-     1.String.prototype.charCodeAt() 返回字符串指定位置的Unicode码点(十进制表示) 只返回两个字节的字符的码点
-               如果遇到码点大于0xFFFF，连续使用charCodeAt(i),charCodeAt(i+1); 
-     2.String.prototype.charAt() 返回指定位置的字符  [];                        
+ * 
+ 
+  
+ 1.String.prototype.charCodeAt(字符) 返回十进制。码点大于0xFFFF，使用charCodeAt(i),charCodeAt(i+1); 
+ 2.String.fromCharCode(数字1,数字2....) 返回拼接好的字符串。码点大于0xFFFF的字符，可以拆分为多个字节合起来表示;
+ 
+   //3和4完美解决1和2的问题
+ 3.String.fromCodePoint(0x20BB7)    
+ 4.String.prototype.codePointAt('𠮷'); 
+
+ 5.String.prototype.charAt()返回指定位置的字符  [];                        
      
  */
-{    
-    console.log(String.fromCharCode(0x0041));//A
-    console.log(String.fromCharCode(104, 101, 108, 108, 111)); //hello
-    let str = '𠮷';// 0x20bb7=>0xd842 0xdfb7(55362 57271)
-    console.log(str.charCodeAt(0), str.charCodeAt(1)); //55362 57271 =>\u0d842 \u0dfb7
+let m = 'hello';
+let str = '𠮷'; // 0x20bb7=>0xd842 0xdfb7(55362 57271)
+{
+    console.log(str.codePointAt(0)); // 134071
+    console.log(String.fromCodePoint(str.codePointAt(0))); //𠮷
+
+    console.log(str.charCodeAt(0), str.charCodeAt(1)); //55362 57271
     console.log(String.fromCharCode(str.charCodeAt(0), str.charCodeAt(1))); //𠮷
+
+    console.log(m.charAt(1), m[1]); //e e
 }
 
 {
-    let str = 'hello';
-    console.log(str.charAt(1), str[1]); //e
+    //不改变原字符 [)
+    console.log('JavaScript'.slice(1, 4)); // ava
+    //不改变原字符 [ 截取字符串长度
+    console.log('JavaScript'.substr(1, 4)); // avaS
 }
