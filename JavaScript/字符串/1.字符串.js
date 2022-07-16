834df225a63712a2245e0adfb5b9e899bad09cb9
liuyuanyuan <1445179059@qq.com>
2022-07-16 17:19:59 +0800

    Unicode编码

--- JavaScript/字符串/1.字符串.js
+++ JavaScript/字符串/1.字符串.js
@@ -1,10 +1,15 @@
 /**
  * 静态方法
-     1.String.fromCharCode() 参数是一个或多个Unicode码点，返回这些组成的字符串。
-      注意，该方法不支持 Unicode 码点大于0xFFFF的字符，即传入的参数不能大于0xFFFF（即十进制的65535)
+     1.String.fromCharCode() 参数Unicode码点，返回这些组成的字符串。不支持码点大于0xFFFF的字符。
+       可以拆分为多个字节表示  𠮷:0x20bb7=>0xd842 0xdfb7
+     
  */
 console.log(String.fromCharCode()); // ""
 console.log(String.fromCharCode(97)); // "a"
 console.log(String.fromCharCode(104, 101, 108, 108, 111)); //hello
+
+
+console.log(String.fromCharCode(0x20bb7)); //乱码
+console.log(String.fromCharCode(0xd842, 0xdfb7)); //正常
 
 
